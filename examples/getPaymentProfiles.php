a68785a0f09197997bba34c301163dad211dbf0e
Arturek1 <artur.programista2@gmail.com>
2025-02-05 04:36:16 -0500

    getPaymentProfiles.php in examples updated

--- examples/getPaymentProfiles.php
+++ examples/getPaymentProfiles.php
@@ -5,3 +5,9 @@ require '../vendor/autoload.php';
 use FourOver\FourOverApiClient;
 
 $client = new FourOverApiClient('PUBLIC_KEY', 'PRIVATE_KEY', 'SANDBOX');
+
+$paymentProfiles = $client->paymentProfiles->getPaymentProfiles();
+
+print_r(
+    $paymentProfiles
+);
