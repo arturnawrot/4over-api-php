6aa660f3a4f572bb8f02ebc3f023158798eb8396
Artur Nawrot <32748505+arturnawrot@users.noreply.github.com>
2025-09-06 15:59:14 -0400

    Redundant inline "true : false" removed.

--- src/BaseApiClient.php
+++ src/BaseApiClient.php
@@ -105,7 +105,7 @@ class BaseApiClient {
      */
     public function isSandboxMode() : bool
     {
-        return $this->getEnvironment() === 'SANDBOX' ? true : false;
+        return $this->getEnvironment() === 'SANDBOX';
     }
 
     /**
